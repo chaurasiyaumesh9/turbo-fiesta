5ed41147342b19f13eb9f5b4e1d3ad728195234c
umesh c <chaurasiyaumesh9@gmail.com>
2019-12-05 18:39:37 +0530

    heroku build try#3

--- server/server.ts
+++ server/server.ts
@@ -8,12 +8,15 @@ const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, "../../client/build")));
